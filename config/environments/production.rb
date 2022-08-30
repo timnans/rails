fbe13e050df6867e3f1eac856dcac0159eeaecdf
sheni <timnans@live.com>
2022-08-30 15:15:06 +0100

    added jost name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.hosts << "rails-production-8de8.up.railway.app"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
